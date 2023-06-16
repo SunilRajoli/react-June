0a61917e2b5d7222f38af9837d87280428259790
SunilRajoli <rajolisunilkumar@gmail.com>
2023-06-16 11:19:16 +0530

    codee

---
+++ Backup/Class-3/src/App.js
@@ -0,0 +1,17 @@
+import Header from "./components/Header";
+import Body from "./components/Body";
+import Message from "./components/Message";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>App Component</h1>
+      <Header /> <hr />
+      <Body /><hr />
+      <Message />
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ Backup/Class-3/src/components/Message.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+class Message extends React.Component {
+    constructor(props){
+        super(props)
+        console.log("First const")
+    }
+        edetails = {"sal": 45000, status: "single"}
+    render(){
+        return <div>
+            <pre>{JSON.stringify(this.edetails)}</pre>
+            <hr />
+            <button>Test</button>
+        </div>
+    }
+}
+export default Message
